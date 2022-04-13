a068d2b361e1705ea62a3f75502b28ebb092a7b5
Steve Manuel <smanuel@rigetti.com>
2022-04-13 13:13:53 -0600

    fix: DebugOutputFormat to iterate over shots and re-use mapping

--- src/output/debug.rs
+++ src/output/debug.rs
@@ -34,49 +34,47 @@ impl OutputFormat for DebugOutputFormat {
         let mut output = vec![];
         match result {
             ExecutionResult::I8(shots_results) => {
-                let mut shot_idx = 0;
-                for recorded_output in mapping {
-                    let shot_id = shot_idx + 1;
-                    match recorded_output {
-                        RecordedOutput::ShotStart => {
-                            output.push(format!("[shot:{} start]", shot_id));
-                        }
-                        RecordedOutput::ShotEnd => {
-                            output.push(format!("[shot:{} end]", shot_id));
-                            shot_idx += 1;
-                        }
-                        RecordedOutput::ResultReadoutOffset(index) => {
-                            // TODO: determine if we need the upstream `index` to be 64-bit.
-                            #[allow(clippy::cast_possible_truncation)]
-                            let index = *index as usize;
-                            if let Some(result) = shots_results
-                                .get(shot_idx)
-                                .and_then(|shot_results| shot_results.get(index))
-                            {
-                                output.push(format!("[shot:{} result {}]", shot_id, result));
-                            } else {
-                                return Err(Error::NoShotDataAtIndex(shot_id, index));
+                for (shot_idx, shot) in shots_results.iter().enumerate() {
+                    for recorded_output in mapping {
+                        let shot_id = shot_idx + 1;
+                        match recorded_output {
+                            RecordedOutput::ShotStart => {
+                                output.push(format!("[shot:{} start]", shot_id));
                             }
-                        }
-                        RecordedOutput::BoolReadoutOffset(..)
-                        | RecordedOutput::IntegerReadoutOffset(..)
-                        | RecordedOutput::DoubleReadoutOffset(..) => {
-                            return Err(Error::UnimplementedRecordType(format!(
-                                "{:?}",
-                                recorded_output
-                            )))
-                        }
-                        RecordedOutput::TupleStart => {
-                            output.push(format!("[shot:{} tuple_start]", shot_id));
-                        }
-                        RecordedOutput::TupleEnd => {
-                            output.push(format!("[shot:{} tuple_end]", shot_id));
-                        }
-                        RecordedOutput::ArrayStart => {
-                            output.push(format!("[shot:{} array_start]", shot_id));
-                        }
-                        RecordedOutput::ArrayEnd => {
-                            output.push(format!("[shot:{} array_end]", shot_id));
+                            RecordedOutput::ShotEnd => {
+                                output.push(format!("[shot:{} end]", shot_id));
+                                break;
+                            }
+                            RecordedOutput::ResultReadoutOffset(index) => {
+                                // TODO: determine if we need the upstream `index` to be 64-bit.
+                                #[allow(clippy::cast_possible_truncation)]
+                                let index = *index as usize;
+                                if let Some(result) = shot.get(index) {
+                                    output.push(format!("[shot:{} result {}]", shot_id, result));
+                                } else {
+                                    return Err(Error::NoShotDataAtIndex(shot_id, index));
+                                }
+                            }
+                            RecordedOutput::BoolReadoutOffset(..)
+                            | RecordedOutput::IntegerReadoutOffset(..)
+                            | RecordedOutput::DoubleReadoutOffset(..) => {
+                                return Err(Error::UnimplementedRecordType(format!(
+                                    "{:?}",
+                                    recorded_output
+                                )))
+                            }
+                            RecordedOutput::TupleStart => {
+                                output.push(format!("[shot:{} tuple_start]", shot_id));
+                            }
+                            RecordedOutput::TupleEnd => {
+                                output.push(format!("[shot:{} tuple_end]", shot_id));
+                            }
+                            RecordedOutput::ArrayStart => {
+                                output.push(format!("[shot:{} array_start]", shot_id));
+                            }
+                            RecordedOutput::ArrayEnd => {
+                                output.push(format!("[shot:{} array_end]", shot_id));
+                            }
                         }
                     }
                 }
@@ -99,34 +97,18 @@ impl From<DebugOutputFormat> for String {
 
 #[test]
 fn test_execution_result_debug_output() {
-    let execution_result = ExecutionResult::I8(vec![
-        vec![1, 2, 3],
-        vec![10, 20, 30, 40],
-        vec![11, 22, 33, 44, 55],
-    ]);
+    let execution_result =
+        ExecutionResult::I8(vec![vec![1, 2, 3], vec![10, 20, 30], vec![11, 22, 33]]);
     let mapping = [
         RecordedOutput::ShotStart,
         RecordedOutput::ResultReadoutOffset(0),
         RecordedOutput::ResultReadoutOffset(1),
         RecordedOutput::ResultReadoutOffset(2),
         RecordedOutput::ShotEnd,
-        RecordedOutput::ShotStart,
-        RecordedOutput::ResultReadoutOffset(0),
-        RecordedOutput::ResultReadoutOffset(1),
-        RecordedOutput::ResultReadoutOffset(2),
-        RecordedOutput::ResultReadoutOffset(3),
-        RecordedOutput::ShotEnd,
-        RecordedOutput::ShotStart,
-        RecordedOutput::ResultReadoutOffset(0),
-        RecordedOutput::ResultReadoutOffset(1),
-        RecordedOutput::ResultReadoutOffset(2),
-        RecordedOutput::ResultReadoutOffset(3),
-        RecordedOutput::ResultReadoutOffset(4),
-        RecordedOutput::ShotEnd,
     ];
 
     let output = DebugOutputFormat::try_new(&execution_result, &mapping).unwrap();
-    assert_eq!(output.0.len(), 18);
+    assert_eq!(output.0.len(), 15);
 
     const EXPECTED_OUTPUT: &str = r#"
 [shot:1 start]
@@ -138,14 +120,11 @@ fn test_execution_result_debug_output() {
 [shot:2 result 10]
 [shot:2 result 20]
 [shot:2 result 30]
-[shot:2 result 40]
 [shot:2 end]
 [shot:3 start]
 [shot:3 result 11]
 [shot:3 result 22]
 [shot:3 result 33]
-[shot:3 result 44]
-[shot:3 result 55]
 [shot:3 end]
 "#;
 
@@ -158,32 +137,19 @@ fn test_execution_result_debug_output() {
 #[test]
 fn test_out_of_range_debug_output() {
     // use misaligned result data with mapping data to trigger `NoShotDataAtIndex` error
-    let execution_result = ExecutionResult::I8(vec![vec![1, 2, 3], vec![10, 20, 30]]);
+    let execution_result = ExecutionResult::I8(vec![vec![1, 2, 3], vec![10, 20]]);
     let mapping = [
         RecordedOutput::ShotStart,
         RecordedOutput::ResultReadoutOffset(0),
         RecordedOutput::ResultReadoutOffset(1),
         RecordedOutput::ResultReadoutOffset(2),
         RecordedOutput::ShotEnd,
-        RecordedOutput::ShotStart,
-        RecordedOutput::ResultReadoutOffset(0),
-        RecordedOutput::ResultReadoutOffset(1),
-        RecordedOutput::ResultReadoutOffset(2),
-        RecordedOutput::ResultReadoutOffset(3),
-        RecordedOutput::ShotEnd,
-        RecordedOutput::ShotStart,
-        RecordedOutput::ResultReadoutOffset(0),
-        RecordedOutput::ResultReadoutOffset(1),
-        RecordedOutput::ResultReadoutOffset(2),
-        RecordedOutput::ResultReadoutOffset(3),
-        RecordedOutput::ResultReadoutOffset(4),
-        RecordedOutput::ShotEnd,
     ];
 
     let try_output = DebugOutputFormat::try_new(&execution_result, &mapping);
     if let Some(Error::NoShotDataAtIndex(shot_id, index)) = try_output.err() {
         assert_eq!(shot_id, 2);
-        assert_eq!(index, 3)
+        assert_eq!(index, 2)
     } else {
         assert!(false);
     }
